2bbcd7a8afa64b2256e84b7e71c028533f215481
stormy/cli <chengcli@umich.edu>
2025-09-30 22:04:54 -0400

    wip

--- earth/fetch_white_sand_dem.sh
+++ earth/fetch_white_sand_dem.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
 # Fetch white sand topography data from google drive
-# Folder ULR = https://drive.google.com/drive/folders/1UarL-r8H1fGL-v0rP7Ze8ezCZEbCiU0-
-URL="https://drive.google.com/uc?id=1GXpDXq9oMa-nZVQaCcBBqaePgwntcAcj"
+#
+# topo_24kmx12km_32.7719_32.9881_-106.5098_-106.3802_adjusted.pt (57 M)
+#URL="https://drive.google.com/uc?id=1GXpDXq9oMa-nZVQaCcBBqaePgwntcAcj"
+#
+URL="https://drive.google.com/uc?id=1n-0od5ytAzW5M0_4rC6kMBn3aPv8NsEm"
 
 echo "Downloading from Google Drive..."
 gdown "$URL"
--- earth/white_sand_topo.py
+++
@@ -1,3 +0,0 @@
-
-def load_white_sand_topo():
-    pass
