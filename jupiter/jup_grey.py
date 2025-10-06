fee56616127ee04cd48eec1c17768eb9a1d641fa
mac/cli <chengcli@umich.edu>
2025-10-06 01:19:38 -0400

    wip

--- jupiter/jup_grey.py
+++ jupiter/jup_grey.py
@@ -27,7 +27,11 @@ class JupGasVisible(torch.nn.Module):
         super().__init__()
         self.scale = scale
 
-    def forward(self, conc, temp, pres, dens) -> torch.Tensor:
+    def forward(self, 
+                conc: torch.Tensor,
+                temp: torch.Tensor,
+                pres: torch.Tensor, 
+                dens: torch.Tensor) -> torch.Tensor:
         """
         Args:
             conc (torch.Tensor): shape (ncol, nlyr, nspecies) [mol/m^3]
@@ -50,33 +54,15 @@ class JupGasVisible(torch.nn.Module):
         return self.scale * dens * (strong_ch4 + weak_ch4).unsqueeze(0).unsqueeze(-1)
 
 class JupGasIR(torch.nn.Module):
-    """
-    GreyOpacity class for handling grey opacity calculations.
-
-    The class signature and variable dimensions are strict.
-    The class must have a forward method that takes a concentration tensor
-    and returns an opacity tensor.
-
-    The concentration vector is 3D with dimensions (ncol, nlyr, nspecies),
-    where ncol is the number of columns, nlyr is the number of layers,
-    and nspecies is the number of species.
-
-    The opacity tensor is 4D with dimensions (nwave, ncol, nlyr, nprop),
-    where nwave is the number of wavelengths, ncol is the number of columns,
-    nlyr is the number of layers, and nprop is the number of optical properties.
-
-    The first optical property is the total extinction cross-section [m^2/mol].
-    The second optical property is the single scattering albedo.
-    Starting from the third, the optical properties are phase function moments
-    (excluding the zero-th moment).
-
-    This class is later compiled to a TorchScript file using the `pyharp.compile` function.
-    """
     def __init__(self, scale: float=1.0):
         super().__init__()
         self.scale = scale
 
-    def forward(self, conc, temp, pres, dens) -> torch.Tensor:
+    def forward(self, 
+                conc: torch.Tensor, 
+                temp: torch.Tensor, 
+                pres: torch.Tensor, 
+                dens: torch.Tensor) -> torch.Tensor:
         """
         Args:
             conc (torch.Tensor): shape (ncol, nlyr, nspecies) [mol/m^3]
