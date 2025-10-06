ec8c55a61eebb283d9aafe8d16cd5b7e3190c4fd
Cheng Li <69489965+chengcli@users.noreply.github.com>
2025-10-05 22:43:05 -0400

    Add saturn case (#2)

---
+++ saturn/plot_sat_profile.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+sat_pseudo = np.genfromtxt("saturn_profile_pseudo_3gpa.txt")
+sat_dry = np.genfromtxt("saturn_profile_dry_3gpa.txt")
+sat_neutral = np.genfromtxt("saturn_profile_neutral_3gpa.txt")
+
+fig = plt.figure(figsize=(6,8))
+ax = fig.add_subplot(1,1,1)
+
+# Plot pseudo adiabat
+pres = sat_pseudo[:,2]
+temp = sat_pseudo[:,3]
+ax.plot(temp, pres, label="Pseudo Adiabat", color='orange')
+
+# Plot dry adiabat
+pres = sat_dry[:,2]
+temp = sat_dry[:,3]
+ax.plot(temp, pres, label="Dry Adiabat", color='green')
+
+# Plot neutral density
+pres = sat_neutral[:,2]
+temp = sat_neutral[:,3]
+ax.plot(temp, pres, label="Neutral", color='red')
+
+# legend
+ax.legend()
+
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_xlabel("Temperature (K)")
+ax.set_ylabel("Pressure (bar)")
+ax.set_ylim(30000, 0.1)
+
+plt.savefig("saturn_profile_3gpa.png", dpi=300)
---
+++ saturn/run_sat_profile_ideal_gas.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+
+from snapy import (
+        MeshBlockOptions,
+        MeshBlock,
+        )
+from kintera import ThermoX
+from paddle import (
+        setup_profile,
+        write_profile,
+        find_init_params,
+        )
+
+def setup_saturn_profile(fname: str, method: str = "moist-adiabat"):
+    print(f"Reading input file: {fname}")
+
+    op_block = MeshBlockOptions.from_yaml(str(fname))
+    block = MeshBlock(op_block)
+
+    # Initial guess for parameters
+    param = {
+        "Ts": 2500.,
+        "Ps": 3.e9,
+        "Tmin": 85.,
+        "xH2O": 8.91e-3,
+        "xNH3": 3.52e-4,
+        "xH2S": 8.08e-5,
+        "grav": 10.44,
+    }
+
+    param = find_init_params(
+            block,
+            param,
+            target_T=134.,
+            target_P=1.e5,
+            method=method,
+            max_iter=100,
+            ftol=1.e-1,
+            verbose=True)
+
+    w = setup_profile(block, param, method=method)
+
+    return w, block
+
+if __name__ == "__main__":
+    # Fix 1bar temperature to 134K
+    print("Setting up Saturn pseudo-adiabat profiles")
+    w, block = setup_saturn_profile("saturn1d.yaml", "pseudo-adiabat")
+    write_profile(f"saturn_profile_pseudo_3gpa.txt", w, block)
+
+    print("Setting up Saturn dry-adiabat profiles")
+    w, block = setup_saturn_profile("saturn1d.yaml", "dry-adiabat")
+    write_profile(f"saturn_profile_dry_3gpa.txt", w, block)
+
+    print("Setting up Saturn neutral density profiles")
+    w, block = setup_saturn_profile("saturn1d.yaml", "neutral")
+    write_profile(f"saturn_profile_neutral_3gpa.txt", w, block)
+
+    print("Saturn profiles setup complete.")
